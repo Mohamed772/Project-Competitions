7e2d051f1a617682f4c7e94095956b2abc2ac04d
Mohamed772 <mohamed-siso@hotmail.fr>
2019-10-16 22:40:39 +0200

    Sprint 4 fini

--- IAProjet1-master/IAP Project4/sprint4.c
+++ IAProjet1-master/IAP Project4/sprint4.c
@@ -10,9 +10,6 @@
 
 #pragma warning(disable : 4996)
 
-
-
-
 typedef struct {
 	float temps;
 } Mesure;
@@ -37,7 +34,7 @@ typedef struct {
 	unsigned int tour_max;
 } Course; //compétition
 
-
+void detection_fin_parcours(Course* liste_equipes);
 
 
 
@@ -57,7 +54,7 @@ void inscrire_equipe(Equipe* e, Course* liste_equipes) {
 		}
 
 		liste_equipes->equipes[liste_equipes->compteur_nb_equipes] = *e;
-		liste_equipes->compteur_nb_equipes++;
+		liste_equipes->compteur_nb_equipes+=1;
 	}
 	else
 		printf("Vous avez déjà atteint le nombre d'équipe maximale");
@@ -65,7 +62,7 @@ void inscrire_equipe(Equipe* e, Course* liste_equipes) {
 
 void afficher_equipes(const Course* liste_equipes) {
 	Equipe e;
-	for (int i = 0; i < liste_equipes->compteur_nb_equipes; i++) {
+	for (unsigned int i = 0; i < liste_equipes->compteur_nb_equipes; i++) {
 		e = liste_equipes->equipes[i];
 		printf("%s %s %u %s %u %s %u\n", e.pays, e.personnes[0].nom, e.personnes[0].num_dossard, e.personnes[1].nom,
 			e.personnes[1].num_dossard, e.personnes[2].nom, e.personnes[2].num_dossard);
@@ -90,13 +87,19 @@ void enregistrement_temps(Course* liste_equipes) {
 	float temps;
 
 	scanf("%u %u %f", &dossard, &num_tours, &temps);
-	if (num_tours <= MAX_TOUR) {
+	if (num_tours <= liste_equipes->tour_max) {
 		trouver_dossard(dossard, &num_equipe, &num_patineur);
 
 		liste_equipes->equipes[num_equipe].personnes[num_patineur].tours = num_tours;
 		liste_equipes->equipes[num_equipe].personnes[num_patineur].liste_temps[num_tours - 1].temps = temps;
 	}
 	else printf("Heu ca fais pas un peu beaucoup la non ?");
+
+	liste_equipes->equipes[num_equipe].tours = compare_tour_equipe(liste_equipes->equipes[num_equipe]);
+	liste_equipes->equipes[num_equipe].liste_temps[num_tours - 1].temps = 0;
+	temps = compare_temps_joueurs(&liste_equipes->equipes[num_equipe], num_tours);
+	liste_equipes->equipes[num_equipe].liste_temps[num_tours - 1].temps = temps;
+	detection_fin_parcours(liste_equipes);
 }
 
 void affichage_temps(const Course* liste_equipes) {
@@ -118,7 +121,7 @@ void affichage_temps(const Course* liste_equipes) {
 	}
 }
 
-int compare_tour_equipe(Equipe equipe) {
+unsigned int compare_tour_equipe(Equipe equipe) {
 	unsigned int a = 0; a = equipe.personnes[0].tours;
 	unsigned int b = 0; b = equipe.personnes[1].tours;
 	unsigned int c = 0; c = equipe.personnes[2].tours;
@@ -126,7 +129,8 @@ int compare_tour_equipe(Equipe equipe) {
 	return ((a <= b) && (a <= c)) ? a : b; 
 	return c;
 }
-float compare_temps_equipe(Equipe* equipe, int tour_actuel) {
+float compare_temps_joueurs(Equipe* equipe, int tour_actuel) {
+	tour_actuel--;
 	if ((equipe->personnes[0].liste_temps[tour_actuel].temps >= equipe->personnes[1].liste_temps[tour_actuel].temps) 
 		&& (equipe->personnes[0].liste_temps[tour_actuel].temps >= equipe->personnes[2].liste_temps[tour_actuel].temps)) {
 		return equipe->personnes[0].liste_temps[tour_actuel].temps;
@@ -138,34 +142,42 @@ float compare_temps_equipe(Equipe* equipe, int tour_actuel) {
 	
 }
 
-void affichage_temps_equipes(Course* liste_equipes) {
-	
-
-	for (int i = 0; i < liste_equipes->compteur_nb_equipes /2; i +=2){
+void affichage_temps_equipes(Course* liste_equipes, unsigned int nb_equipes) {
+	for (unsigned int i = 0; i < nb_equipes; i+=2 ){
 		int tour_actuel = 0;
 		int equipe1 = compare_tour_equipe(liste_equipes->equipes[i]),
 			equipe2 = compare_tour_equipe(liste_equipes->equipes[i+1]);
 
+		liste_equipes->equipes[i].tours = equipe1;
+		liste_equipes->equipes[i+1].tours = equipe2;
 
-		if (equipe1 >= equipe2) {
-			tour_actuel = equipe2;
-		}
+		if (equipe1 >= equipe2) { tour_actuel = equipe2; }
 		else tour_actuel = equipe1;
 
-		liste_equipes->equipes[i].liste_temps[tour_actuel].temps = compare_temps_equipe(&liste_equipes->equipes[i], tour_actuel);
-		liste_equipes->equipes[i + 1].liste_temps[tour_actuel].temps = compare_temps_equipe(&liste_equipes->equipes[i + 1], tour_actuel);
+		liste_equipes->equipes[i].liste_temps[tour_actuel-1].temps = compare_temps_joueurs(&liste_equipes->equipes[i], tour_actuel);
+		liste_equipes->equipes[i + 1].liste_temps[tour_actuel-1].temps = compare_temps_joueurs(&liste_equipes->equipes[i + 1], tour_actuel);
 
-		printf("%s %.1f\n", liste_equipes->equipes[i].pays, liste_equipes->equipes[i].liste_temps[tour_actuel].temps);
-		printf("%s %.1f\n", liste_equipes->equipes[i+1].pays, liste_equipes->equipes[i].liste_temps[tour_actuel].temps);
+		printf("%s %.1f\n", liste_equipes->equipes[i].pays, liste_equipes->equipes[i].liste_temps[tour_actuel-1].temps);
+		printf("%s %.1f\n", liste_equipes->equipes[i+1].pays, liste_equipes->equipes[i+1].liste_temps[tour_actuel-1].temps);
 
 	}
-	
-
-	
 }
 
+void detection_fin_parcours(Course* liste_equipes) {
+	unsigned int tour_max = liste_equipes->tour_max;
+	for (unsigned int i = 0; i < liste_equipes->compteur_nb_equipes; i += 2) {
+		if ((liste_equipes->equipes[i].tours  == tour_max) && (liste_equipes->equipes[i + 1].tours == tour_max)) {
+			printf("Detection_fin_parcours\n");
+			printf("%s %.1f\n", liste_equipes->equipes[i].pays, liste_equipes->equipes[i].liste_temps[tour_max - 1].temps);
+			printf("%s %.1f\n", liste_equipes->equipes[i + 1].pays, liste_equipes->equipes[i + 1].liste_temps[tour_max - 1].temps);
+		}
+	}
+}
+
+
 int main() {
-	Course liste_equipes; liste_equipes.compteur_nb_equipes = 0;
+	Course liste_equipes; 
+	liste_equipes.compteur_nb_equipes = 0;
 	Equipe e;
 	char mot[lgMot + 1];
 
@@ -184,7 +196,13 @@ int main() {
 			affichage_temps(&liste_equipes);
 		}
 		if (strcmp(mot, "afficher_temps_equipes") == 0) {
-			affichage_temps_equipes(&liste_equipes);
+			unsigned int nb_equipes = liste_equipes.compteur_nb_equipes;
+			affichage_temps_equipes(&liste_equipes, nb_equipes);
+		}
+		if (strcmp(mot, "definir_parcours") == 0) {
+			unsigned int parcours;
+			scanf("%u", &parcours);
+			liste_equipes.tour_max = parcours;
 		}
 	} while (strcmp(mot, "exit") != 0);
 	exit(0);
